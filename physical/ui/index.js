63488a59f0610836ef93e24364027616c8e9525d
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2016-09-08 19:18:28 +0200

    Add support for buttons that are connected to pull-down pins
    These buttons require the pull-down resistor to be enabled
    on the Raspberry Pi so patched versions of johnny-five and
    raspi-io are included.

--- physical/ui/index.js
+++ physical/ui/index.js
@@ -1,14 +1,60 @@
 var five = require('johnny-five');
 var EchoIO = require('./echo-io');
 
+var IO = null;
+
+try {
+  IO = require('raspi-io');
+} catch(err) {
+  console.error('Raspi-io not found, falling back to EchoIO');
+  console.error(err);
+  IO = EchoIO;
+}
+
+var uiConfig = require('../../config/physical-config.json');
+
 module.exports.create = function () {
+  const io = new IO();
+
   var board = new five.Board({
-    io: new EchoIO()
+    io: io
   });
+
+  var instances = {
+    Button: {}
+  };
 
   board.on('ready', function() {
     console.log('Board is ready');
 
-    
+    // instances.button = new five.Button({ pin: 7, invert: false, pulldown: true });
+    //
+    // instances.button.on('press', () => console.log('pressed!'));
+
+    uiConfig.Button.map(function (btn) {
+      const id = btn.id;
+      const config = btn.config;
+
+      const button = new five.Button(Object.assign({ id: id }, config));
+
+      button.on("hold", function() {
+         console.log( id + ": Button held" );
+       });
+
+       button.on("press", function() {
+         console.log( id + ": Button pressed" );
+       });
+
+       button.on("release", function() {
+         console.log( id + ": Button released" );
+       });
+
+      instances.Button[id] = button;
+    });
+
+    this.repl.inject(Object.assign(
+      { io: io, five: five },
+      instances
+    ));
   });
 }
